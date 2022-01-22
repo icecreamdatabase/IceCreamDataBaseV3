0e4294f05468ce7a89253cc583a0550b4ff56f99
Karl <git@karlneugebauer.com>
2022-01-22 12:33:32 +0100

    refactoring

--- IceCreamDataBaseV3/Bot.cs
+++ IceCreamDataBaseV3/Bot.cs
@@ -2,30 +2,27 @@
 using TwitchIrcHubClient;
 using TwitchIrcHubClient.DataTypes.Parsed.FromTwitch;
 using TwitchIrcHubClient.DataTypes.Parsed.ToTwitch;
-using TwitchIrcHubClient.InternalApi.Connections;
-using TwitchIrcHubClient.InternalApi.TwitchUsers;
 
 namespace IceCreamDataBaseV3;
 
 public class Bot
 {
-    private readonly IrcHubClient _ircHubClient;
+    private readonly IrcHubClient _hub;
 
     public Bot()
     {
         if (string.IsNullOrEmpty(Program.ConfigRoot.TwitchIrcHub.AppIdKey))
             throw new InvalidOperationException("No AppIdKey!");
-        GlobalDataSetup.AppIdKey = Program.ConfigRoot.TwitchIrcHub.AppIdKey;
 
-        _ircHubClient = new IrcHubClient(Program.ConfigRoot.TwitchIrcHub.AppIdKey);
-        _ircHubClient.IncomingIrcEvents.OnNewIrcPrivMsg += OnNewIrcPrivMsg;
-        _ircHubClient.IncomingIrcEvents.OnConnId += OnConnId;
+        _hub = new IrcHubClient(Program.ConfigRoot.TwitchIrcHub.AppIdKey);
+        _hub.IncomingIrcEvents.OnNewIrcPrivMsg += OnNewIrcPrivMsg;
+        _hub.IncomingIrcEvents.OnConnId += OnConnId;
         var a = IcdbDbContext.Instance;
     }
 
     private void OnConnId(string connid)
     {
-        Connections.SetChannels(122425204, new List<int> { 38949074 });
+        _hub.Api.Connections.SetChannels(122425204, new List<int> { 38949074 });
     }
 
     private async void OnNewIrcPrivMsg(IrcPrivMsg ircPrivMsg)
@@ -34,12 +31,12 @@ public class Bot
         if (ircPrivMsg.RoomId == 38949074)
             if (ircPrivMsg.Message.StartsWith("<"))
             {
-                await _ircHubClient.OutgoingIrcEvents.SendPrivMsg(new PrivMsgToTwitch(122425204, "icdb", ">"));
+                await _hub.OutgoingIrcEvents.SendPrivMsg(new PrivMsgToTwitch(122425204, "icdb", ">"));
             }
             else if (ircPrivMsg.Message.StartsWith(">"))
             {
-                Dictionary<string, string> dict = await TwitchUsers.IdToLogin(new[] { 38949074 });
-                await _ircHubClient.OutgoingIrcEvents.SendPrivMsg(new PrivMsgToTwitch(122425204, "icdb", "xd"));
+                Dictionary<string, string> dict = await _hub.Api.TwitchUsers.IdToLogin(new[] { 38949074 });
+                await _hub.OutgoingIrcEvents.SendPrivMsg(new PrivMsgToTwitch(122425204, "icdb", "xd"));
             }
     }
 }
